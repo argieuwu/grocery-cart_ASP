84a05689472e538be558bd98bfd7d26dc22b6d09
Argiee <112626873+Argiezuu@users.noreply.github.com>
2024-10-29 20:07:33 +0800

    adding mvc

--- SuperMarket/Controllers/HomeController.cs
+++ SuperMarket/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace SuperMarket.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
+        } 
 
         public IActionResult Index()
         {
---
+++ SuperMarket/Models/CategoryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperMarket.Models
+{
+    public class CategoryModel
+    {
+        public int CategoryId { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+    }
+}
