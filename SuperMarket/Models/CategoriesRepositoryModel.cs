df28c3ef32251472f16dba104f217f952412a889
Argiee <112626873+Argiezuu@users.noreply.github.com>
2024-11-01 18:13:24 +0800

    adding categoriesRepository for CRUD

---
+++ SuperMarket/Models/CategoriesRepositoryModel.cs
@@ -0,0 +1,59 @@
+﻿namespace SuperMarket.Models
+{
+    public class CategoriesRepositoryModel
+    {
+        private static List<CategoryModel> _categories = new List<CategoryModel>()
+        {
+            new CategoryModel {CategoryId = 1, Name = "ratatowe", Description = "bombay"},
+            new CategoryModel {CategoryId = 2, Name = "tatowe", Description = "bay"},
+            new CategoryModel {CategoryId = 3, Name = "towe", Description = "y"}
+        };
+
+        public static void AddCategory (CategoryModel category)
+        {
+            var maxId = _categories.Max(x => x.CategoryId);
+            category.CategoryId = maxId + 1;
+            _categories.Add(category);
+        }
+
+        public static List<CategoryModel> GetCategoryModels() => _categories;
+
+        public static CategoryModel? GetCategoryById(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
+            if(category != null)
+            {
+                return new CategoryModel
+                {
+                    CategoryId = category.CategoryId,
+                    Name = category.Name,
+                    Description = category.Description
+                };
+            }
+            return null;
+        }
+
+        public static void UpdateCategory(int categoryId, CategoryModel category)
+        {
+            if (categoryId != category.CategoryId) return;
+
+            var categoryToUpdate = GetCategoryById(categoryId);
+
+            if (categoryToUpdate != null)
+            {
+                categoryToUpdate.Name = category.Description;
+                categoryToUpdate.Description = category.Description;
+            }
+        }
+
+        public static void DeleteCategory(int categoryId)
+        {
+            var category = _categories.FirstOrDefault(x => x.CategoryId == categoryId);
+
+            if (category != null)
+            {
+                _categories.Remove(category);
+            }
+        }
+    }
+}
