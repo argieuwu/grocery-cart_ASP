441a491225027cbffb525906745991e27457f215
Argiee <112626873+Argiezuu@users.noreply.github.com>
2024-11-08 22:48:19 +0800

    ywaa ani uys

--- SuperMarket/Controllers/AuthPageController.cs
+++ SuperMarket/Controllers/AuthPageController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GeoceriCart.Controllers
 {
@@ -36,6 +38,12 @@ namespace GeoceriCart.Controllers
                 ViewBag.ErrorMessage = "Invalid username or password";
                 return View();
             }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
